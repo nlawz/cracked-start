d3801631de8352d1cb69e1d5a72a2ff86872bd58
n <nlaw.dev@gmail.com>
2024-12-12 12:24:00 +0700

    wrapped autherror in suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,52 +9,62 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
-
+import { Suspense } from "react";
 import ProviderSigninBlock from "@/components/ProviderSigninBlock";
 import LoginForm from "@/components/LoginForm";
 import { AuthError } from "@/components/AuthError";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-center bg-muted min-h-screen">
-      <Card className="w-[350px] mx-auto">
-        <AuthError />
-        <CardHeader className="space-y-1">
-          <div className="flex justify-center py-4">
-            <Link href="/">
-              <Image src="/cracked.svg" alt="logo" width={50} height={50} />
-            </Link>
-          </div>
+      <div className="flex items-center justify-center bg-muted min-h-screen">
+          <Card className="w-[350px] mx-auto">
+              <Suspense fallback={null}>
+                  <AuthError />
+              </Suspense>
+              <CardHeader className="space-y-1">
+                  <div className="flex justify-center py-4">
+                      <Link href="/">
+                          <Image
+                              src="/cracked.svg"
+                              alt="logo"
+                              width={50}
+                              height={50}
+                          />
+                      </Link>
+                  </div>
 
-          <CardTitle className="text-2xl font-bold">Login</CardTitle>
-          <CardDescription>Choose your preferred login method</CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <LoginForm />
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
-          <ProviderSigninBlock />
-        </CardContent>
-        <CardFooter className="flex-col text-center">
-          <Link
-            className="w-full text-sm text-muted-foreground "
-            href="/forgot-password"
-          >
-            Forgot password?
-          </Link>
-          <Link className="w-full text-sm text-muted-foreground" href="/signup">
-            Don&apos;t have an account? Signup
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
+                  <CardTitle className="text-2xl font-bold">Login</CardTitle>
+                  <CardDescription>
+                      Choose your preferred login method
+                  </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-4">
+                  <LoginForm />
+                  <div className="relative">
+                      <div className="absolute inset-0 flex items-center">
+                          <span className="w-full border-t" />
+                      </div>
+                      <div className="relative flex justify-center text-xs uppercase">
+                          <span className="bg-background px-2 text-muted-foreground">
+                              Or continue with
+                          </span>
+                      </div>
+                  </div>
+                  <ProviderSigninBlock />
+              </CardContent>
+              <CardFooter className="flex-col text-center">
+                  <Link
+                      className="w-full text-sm text-muted-foreground "
+                      href="/forgot-password">
+                      Forgot password?
+                  </Link>
+                  <Link
+                      className="w-full text-sm text-muted-foreground"
+                      href="/signup">
+                      Don&apos;t have an account? Signup
+                  </Link>
+              </CardFooter>
+          </Card>
+      </div>
   );
 }
